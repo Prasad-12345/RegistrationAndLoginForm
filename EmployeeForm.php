51d85b82a9664ca730aaabf2480fff155ba07bd2
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-07-13 09:44:17 +0530

    Added: Form to add employee

--- EmployeeForm.php
+++ EmployeeForm.php
@@ -16,20 +16,45 @@
         <form action="SQLQuery.php" method="post">
             <input type="text" name="srno" class="form-control" pattern="[0-9]{1,}" placeholder="Enter srno" required><br>
             <input type="text" name="emp_id" class="form-control" pattern="[0-9]{1,}"placeholder="Enter emp id" required><br>
-            <input type="text" name="address" class="form-control" pattern="[0-9a-zA-Z]{3,}"placeholder="Enter address" required><br>
-            <input type="text" name="zipcode" class="form-control" pattern="[0-9]{6}" placeholder="Enter zipcode" required><br>
-            <input type="text" name="salary" class="form-control" pattern="[0-9]{4,}" placeholder="Enter salary" required><br>
+            <input type="text" name="name" class="form-control" placeholder="Enter name" pattern="[A-Za-z]{3,}" title = "Name should have more than two charcters and use only alphabetse.g: Prasad" required><br>
+            <input type="text" name="city" class="form-control" pattern="[a-zA-Z]{3,}"placeholder="Enter city" pattern="[A-Za-z]{3,}" title = "City should have more than two charcters and only alphabets are allowed e.g: Pune"required><br>
+            <input type="text" name="zipcode" class="form-control"  placeholder="Enter zipcode" pattern="[0-9]{6}" title = "Zipcode should be of 6 charcters"required><br>
+            <input type="text" name="salary" class="form-control" pattern="[0-9]{4,}" title = "Use numbers only" placeholder="Enter salary" required><br>
             <input type="submit" class="form-control submit" value="add" name="add">
-        </form>
+        </form>  
     </div>
-        <!-- <div class="container">
-        <button class = "btn btn-primary my-5"><a href="Index.php" class="text-light">Add</a></button> -->
+  
+    <form action="SearchAndSort.php" method="POST">
+        <!-- <button class="btn" value="display" type="submit" name="display">display</button> -->
+
+        <!-- <select name="searchby" class="searchbtn">
+            <option>search by</option>
+            <option value="name" >name</option>
+            <option value="city">city</option>
+            <option value="emp_id">emp_id</option>
+        </select> -->
+        <input type="submit" name="searchdata" class="searchdatabtn" value="searchdata">
+        <!-- <button class="searchbtn" type="submit" value="searchdata">searchdata</button> -->
+    </form>
+
+    <form action="" method="post">
+       <!-- <button class="displaybtn" value="display" type="submit" name="display">display</button>  -->
+        <select name="getValue" class="btn">
+          <option>sort by</option>
+          <option value="name" >name</option>
+          <option value="salary">salary</option>
+          <option value="emp_id">emp_id</option>
+        </select>
+        <input type="submit" class="sortbtn" name="sort" value="sort">
+    </form>
+ 
     <table style= "width:70%" class="center">
     <thead>
     <tr>
       <th scope="col" class="column" class="text-light">srno</th>
       <th scope="col" class="column" class="text-light">emp_id</th>
-      <th scope="col" class="column" class="text-light">address</th>
+      <th scope="col" class="column" class="text-light">name</th>
+      <th scope="col" class="column" class="text-light">city</th>
       <th scope="col" class="column" class="text-light">zipcode</th>
       <th scope="col" class="column" class="text-light">salary</th>
       <th scope="col" class="column" class="text-light">Action</th>
@@ -37,20 +62,22 @@
   </thead>
   <tbody>
   <?php include "ConnectToDataBase.php";
-        //$conn = mysqli_connect("localhost:3306", "root", "Prasad@321", "ContactData") or die("Connection Failed");
+      //if(isset($_POST['display'])){
         $sql = "select * from employee_data";
         $result = mysqli_query($conn, $sql);
         if($result){
             while($row=mysqli_fetch_assoc($result)){
                 $srno = $row['srno'];
                 $emp_id = $row['emp_id'];
-                $address = $row['address'];
+                $name = $row['name'];
+                $city = $row['city'];
                 $zipcode = $row['zipcode'];
                 $salary = $row['salary'];
                 echo  '<tr>
                 <th scope="row">'.$srno.'</th>
                 <td>'.$emp_id.'</td>
-                <td>'.$address.'</td>
+                <td>'.$name.'</td>
+                <td>'.$city.'</td>
                 <td>'.$zipcode.'</td>
                 <td>'.$salary.'</td>
                 <td>
@@ -60,8 +87,52 @@
               </tr>';
             }
         }
+        ?>
+  </tbody>
+      
+    <table style= "width:70%" class="center">
+    <thead>
+    <tr>
+      <th scope="col" class="column" class="text-light">srno</th>
+      <th scope="col" class="column" class="text-light">emp_id</th>
+      <th scope="col" class="column" class="text-light">name</th>
+      <th scope="col" class="column" class="text-light">city</th>
+      <th scope="col" class="column" class="text-light">zipcode</th>
+      <th scope="col" class="column" class="text-light">salary</th>
+      <th scope="col" class="column" class="text-light">Action</th>
+    </tr>
+  </thead>
+      <tbody>
+    <?php include "ConnectToDataBase.php";
+      if(isset($_POST['sort'])){
+        $value = $_POST['getValue'];
+        $sql = "select * from employee_data order by $value";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                $srno = $row['srno'];
+                $emp_id = $row['emp_id'];
+                $name = $row['name'];
+                $city = $row['city'];
+                $zipcode = $row['zipcode'];
+                $salary = $row['salary'];
+                echo  '<tr>
+                <th scope="row">'.$srno.'</th>
+                <td>'.$emp_id.'</td>
+                <td>'.$name.'</td>
+                <td>'.$city.'</td>
+                <td>'.$zipcode.'</td>
+                <td>'.$salary.'</td>
+                <td>
+                <button class="editbtn" ><a href="Edit.php?editid='.$srno.'"  class = "text-light">Edit</a></button>
+                <button class="deletebtn"><a href="SQLQuery.php?deleteid='.$srno.'" class = "text-light">Delete</a></button>
+                </td>
+              </tr>';
+            }
+        }
+      }
     ?>
   </tbody>
-</table> 
+</table>  
 </body>
 </html>
